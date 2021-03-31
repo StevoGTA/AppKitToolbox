1be2de80653cd1bd585990e8a72be11e9a072c27
Stevo <stevo@sunsetmagicwerks.com>
2021-03-30 17:44:43 -0700

    Updates based on C++ Toolbox.

--- Source/View/AKTMetalView.mm
+++ Source/View/AKTMetalView.mm
@@ -117,9 +117,7 @@ static	MTLRenderPassDescriptor*	sGetCurrentRenderPassDescriptor(AKTMetalView* me
 	// Run lean
 	@autoreleasepool {
 		// Call periodic proc
-		self.periodicProc(
-				SUniversalTime::getCurrentUniversalTime() +
-						1.0 / (UniversalTimeInterval) self.preferredFramesPerSecond);
+		self.periodicProc(SUniversalTime::getCurrent() + 1.0 / (UniversalTimeInterval) self.preferredFramesPerSecond);
 	}
 }
 
