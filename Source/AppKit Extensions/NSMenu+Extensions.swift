5946d9e0dd0af126b06bc631c7cc93075dd15beb
Stevo <stevo@sunsetmagicwerks.com>
2022-01-26 22:48:29 -0800

    Updated NSMenu extension

--- Source/AppKit Extensions/NSMenu+Extensions.swift
+++ Source/AppKit Extensions/NSMenu+Extensions.swift
@@ -14,10 +14,21 @@ public extension NSMenu {
 
 	// MARK: Instance methods
 	//------------------------------------------------------------------------------------------------------------------
-	func addItem(withTitle title :String, action :Selector, representedObject :Any) {
+	func addItem(withTitle title :String, target :AnyObject? = nil, action :Selector, representedObject :Any? = nil) {
 		// Setup
 		let	menuItem = NSMenuItem(title: title, action: action, keyEquivalent: "")
+		menuItem.target = target
 		menuItem.representedObject = representedObject
+
+		// Add item
+		addItem(menuItem)
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	func addItem(withTitle title :String, menu :NSMenu) {
+		// Setup
+		let	menuItem = NSMenuItem(title: title, action: nil, keyEquivalent: "")
+		menuItem.submenu = menu
 
 		// Add item
 		addItem(menuItem)
