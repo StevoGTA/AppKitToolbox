0934e9820fe95fbaa4905133e4de2ac20caae6a3
Stevo <stevo@sunsetmagicwerks.com>
2025-09-30 10:23:12 -0700

    Renamed AKTProgressViewController+Swift => AKTViewController+Progress.

--- Source/Controller/AKTViewController+Progress.swift
+++ Source/Controller/AKTViewController+Progress.swift
@@ -1,43 +1,8 @@
 //----------------------------------------------------------------------------------------------------------------------
-//	AKTProgressViewController+Swift.swift			©2021 Stevo Brock		All rights reserved.
+//	AKTViewController+Progress.swift			©2021 Stevo Brock		All rights reserved.
 //----------------------------------------------------------------------------------------------------------------------
 
 import Foundation
-
-//----------------------------------------------------------------------------------------------------------------------
-// MARK: AKTProgressViewController extension
-public extension AKTProgressViewController {
-
-	// MARK: Instance methods
-	//------------------------------------------------------------------------------------------------------------------
-	func setup(progress :Progress, cancelProc :CancelProc? = nil) {
-		// Setup
-		progress.updatedProc = { [unowned self] in self.updateUI($0) }
-
-		// Set cancel proc
-		self.cancelProc = cancelProc
-
-		// Update UI
-		updateUI(progress)
-	}
-
-	// MARK: Private methods
-	//------------------------------------------------------------------------------------------------------------------
-	private func updateUI(_ progress :Progress) {
-		// Update UI
-		self.messageLabel.stringValue = progress.message
-
-		self.progressIndicator.isIndeterminate = progress.value == nil
-		self.progressIndicator.doubleValue = progress.value ?? 0.0
-		if progress.value != nil {
-			// Have value
-			self.progressIndicator.stopAnimation(self)
-		} else {
-			// Don't have value
-			self.progressIndicator.startAnimation(self)
-		}
-	}
-}
 
 //----------------------------------------------------------------------------------------------------------------------
 // MARK: AKTViewController extension
@@ -89,7 +54,49 @@ public extension AKTViewController {
 		}
 	}
 
-	// MARK: Instance methods
+	//------------------------------------------------------------------------------------------------------------------
+	func perform<T>(progress :Progress, progressViewController :AKTProgressViewController,
+			procDispatchQueue :DispatchQueue = DispatchQueue.global(qos: .userInitiated),
+			proc :@escaping () -> (result :T?, error :Error?), cancelProc :(() -> Void)? = nil,
+			completionProc :@escaping (_ result :T?, _ error :Error?) -> Void = { _,_ in }) {
+		// Setup
+		var	isCancelled = false
+		if cancelProc != nil {
+			// Have cancelProc
+			progressViewController.setup(progress: progress, cancelProc: {
+				// Cancelled
+				isCancelled = true
+
+				// Call proc
+				cancelProc!()
+			})
+		} else {
+			// Don't have cancelProc
+			progressViewController.setup(progress: progress)
+		}
+
+		// Present progress view controller as sheet
+		presentAsSheet(progressViewController)
+
+		// Jump to procs queue
+		procDispatchQueue.async() {
+			// Call proc
+			let	(result, procError) = proc()
+
+			// Jump to main queue
+			DispatchQueue.main.async() {
+				// Update UI
+				self.dismiss(progressViewController)
+
+				// Check cancelled
+				if !isCancelled {
+					// Call completion proc
+					completionProc(result, procError)
+				}
+			}
+		}
+	}
+
 	//------------------------------------------------------------------------------------------------------------------
 	func perform(progress :Progress, progressViewController :AKTProgressViewController,
 			procDispatchQueue :DispatchQueue = DispatchQueue.global(qos: .userInitiated),
@@ -131,6 +138,41 @@ public extension AKTViewController {
 					completionProc(procError)
 				}
 			}
+		}
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - AKTProgressViewController extension
+fileprivate extension AKTProgressViewController {
+
+	// MARK: Instance methods
+	//------------------------------------------------------------------------------------------------------------------
+	func setup(progress :Progress, cancelProc :CancelProc? = nil) {
+		// Setup
+		progress.updatedProc = { [unowned self] in self.updateUI($0) }
+
+		// Set cancel proc
+		self.cancelProc = cancelProc
+
+		// Update UI
+		updateUI(progress)
+	}
+
+	// MARK: Private methods
+	//------------------------------------------------------------------------------------------------------------------
+	private func updateUI(_ progress :Progress) {
+		// Update UI
+		self.messageLabel.stringValue = progress.message
+
+		self.progressIndicator.isIndeterminate = progress.value == nil
+		self.progressIndicator.doubleValue = progress.value ?? 0.0
+		if progress.value != nil {
+			// Have value
+			self.progressIndicator.stopAnimation(self)
+		} else {
+			// Don't have value
+			self.progressIndicator.startAnimation(self)
 		}
 	}
 }
